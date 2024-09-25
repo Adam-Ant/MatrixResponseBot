007b250d2bfbec78e6ea80caf8d5ec780f24abfc
Adam Dodman <adam@adam-ant.co.uk>
2024-09-26 00:01:42 +0100

    Move to sub commands

--- bot.py
+++ bot.py
@@ -29,45 +29,15 @@ class ResponseBot(Plugin):
     def get_command_name(self) -> str:
         return self.config["command"]
 
+    # TODO: Make these configurable?
     def get_upload_web_name(self) -> str:
-        return f"{self.config['command']}-upload-url"
+        return "upload-url"
 
     def get_dump_mxc_name(self) -> str:
-        return f"{self.config['command']}-dump-mxc"
+        return "dump-mxc"
 
-    @command.new(name=get_upload_web_name)
-    @command.argument("message", pass_raw=True)
-    async def upload_web(self, evt: MessageEvent, message: str) -> None:
-        if evt.sender in self.config["whitelist"]:
-            try:
-                resp = await self.http.get(message)
-            except InvalidURL:
-                await evt.reply("Failed: Invalid URL")
-                return
-            if resp.status == 200:
-                data = await resp.read()
-                file_name = message.split("/")[-1]
-                mime_type = magic.from_buffer(data, mime=True)
-                if "video" in mime_type:
-                    uri = await self.client.upload_media(
-                        data, mime_type=mime_type, filename=file_name
-                    )
-                    if not self.config["urls"]:
-                        self.config["urls"] = []
-                    self.config["urls"].append(uri)
-                    self.config.save()
-                    await evt.reply(f"Added to config. MXC: {uri}")
-                else:
-                    await evt.reply("Failed: URL not a video!")
-            else:
-                await evt.reply(f"Failed: Got Response {resp.status}")
-        else:
-            await evt.reply(
-                "Your mother is a hamster and your father smells of elderberries!"
-            )
-
-    @command.new(name=get_command_name)
-    async def send_video(self, evt: MessageEvent) -> None:
+    @command.new(name=get_command_name, require_subcommand=False)
+    async def bot_command(self, evt: MessageEvent) -> None:
         url = self.config["urls"][randint(0, len(self.config["urls"]) - 1)]
         # This whole thing is such a sodding bodge. Why doesn't matrix store any metadata with its MXC :(
         video = await self.client.download_media(url=url)
@@ -85,14 +55,42 @@ class ResponseBot(Plugin):
         )
         await evt.reply(content)
 
-    @command.new(name=get_dump_mxc_name)
+    @bot_command.subcommand(name=get_dump_mxc_name, help="Admin only: Print the full list of media MXC URLs to this channel")
     async def dump_mxc(self, evt: MessageEvent) -> None:
         if evt.sender in self.config["whitelist"]:
             message = TextMessageEventContent(
                 msgtype=MessageType.TEXT,
                 body=f"MXCs:\n\r{'\n\r'.join(self.config["urls"])}",
             )
-
             await evt.reply(message)
+        else:
+            await evt.react("lol no")
+
+    @bot_command.subcommand(name=get_upload_web_name, help="Admin only: Add a new response from a URL")
+    @command.argument("url", pass_raw=True, required=True)
+    async def upload_web(self, evt: MessageEvent, url: str) -> None:
+        if evt.sender in self.config["whitelist"]:
+            try:
+                resp = await self.http.get(url)
+            except InvalidURL:
+                await evt.reply("Failed: Invalid URL")
+                return
+            if resp.status == 200:
+                data = await resp.read()
+                file_name = url.split("/")[-1]
+                mime_type = magic.from_buffer(data, mime=True)
+                if "video" in mime_type:
+                    uri = await self.client.upload_media(
+                        data, mime_type=mime_type, filename=file_name
+                    )
+                    if not self.config["urls"]:
+                        self.config["urls"] = []
+                    self.config["urls"].append(uri)
+                    self.config.save()
+                    await evt.reply(f"Added to config. MXC: {uri}")
+                else:
+                    await evt.reply("Failed: URL not a video!")
+            else:
+                await evt.reply(f"Failed: Got Response {resp.status}")
         else:
             await evt.react("lol no")
