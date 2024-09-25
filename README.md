e56d5314792b8e430f7348ba05f39d9c4dfe18d2
Adam Dodman <adam@adam-ant.co.uk>
2024-09-25 22:42:51 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Matrix Response Bot
+A simple [maubot](https://github.com/maubot/maubot) that posts random videos in response to a command.
+
+Originally designed as a ~~bad idea~~ way to generate a random [Tom Scott Excuse](https://www.youtube.com/watch?v=_MJRecWakxw)
---
+++ bot.py
@@ -0,0 +1,75 @@
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from mautrix.types import MediaMessageEventContent, MessageType, VideoInfo
+from maubot import Plugin, MessageEvent
+from maubot.handlers import command
+from aiohttp.client_exceptions import InvalidURL
+import magic
+
+from typing import Type
+from random import randint
+
+class Config(BaseProxyConfig):
+  def do_update(self, helper: ConfigUpdateHelper) -> None:
+    helper.copy("whitelist")
+    helper.copy("command")
+    helper.copy("urls")
+    helper.copy("video_height")
+    helper.copy("video_width")
+
+class ResponseBot(Plugin):
+  async def start(self) -> None:
+    self.config.load_and_update()
+
+  @classmethod
+  def get_config_class(cls) -> Type[BaseProxyConfig]:
+    return Config
+  
+  def get_command_name(self) -> str:
+    return self.config["command"]
+  
+  def get_upload_web_name(self) -> str:
+    return f"{self.config['command']}-upload-url"
+
+  @command.new(name=get_upload_web_name)
+  @command.argument("message", pass_raw=True)
+  async def upload_web(self, evt: MessageEvent, message: str) -> None:
+    if evt.sender in self.config["whitelist"]:
+      try:
+        resp = await self.http.get(message)
+      except InvalidURL:
+        await evt.reply("Failed: Invalid URL")
+        return
+      if resp.status == 200:
+        data = await resp.read()
+        file_name = message.split("/")[-1]
+        mime_type = magic.from_buffer(data, mime=True)
+        if "video" in mime_type:
+          uri = await self.client.upload_media(data, mime_type=mime_type, filename=file_name)
+          if not self.config["urls"]:
+              self.config["urls"] = []
+          self.config["urls"].append(uri)
+          self.config.save()
+          await evt.reply(f"Added to config. MXC: {uri}")
+        else:
+          await evt.reply(f"Failed: URL not a video!")
+      else:
+        await evt.reply(f"Failed: Got Response {resp.status}")
+    else:
+       await evt.reply(f"Your mother is a hamster and your father smells of elderberries!")
+
+  @command.new(name=get_command_name)
+  async def test(self, evt: MessageEvent) -> None:
+    url = self.config["urls"][randint(0, len(self.config["urls"])-1)]
+    # This whole thing is such a sodding bodge. Why doesn't matrix store any metadata with its MXC :(
+    video = await self.client.download_media(url=url)
+    mime_type = magic.from_buffer(video, mime=True)
+    content = MediaMessageEventContent(url=url,
+                                       body="Response.mp4",
+                                       msgtype=MessageType.VIDEO,
+                                       info=VideoInfo(
+                                         mimetype=mime_type,
+                                         size = len(video),
+                                         width = self.config["video_width"],
+                                         height = self.config["video_height"]
+                                       ))
+    await evt.reply(content)
